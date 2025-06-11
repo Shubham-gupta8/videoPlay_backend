4223ba4eba4ab1ec6a0291c979b5cae574937a09
Shubham-gupta8 <shubham.work27052004@gmail.com>
2025-06-11 20:06:35 +0000

    express app cors cookie parser middleware

--- src/index.js
+++ src/index.js
@@ -5,7 +5,23 @@ console.log(process.env.MONGODB_URI);
 
 import connectDB from './db/index.js';
 
+// Error event listener any wher if error occur app.emit(error) is given and now app.on will handle that error 
+// basically it is great for production as it keep the error handling centralized but not good for dubugging
+app.on('error', (error) => {
+    console.error("An error occurred before server startup:", error);
+    // You may also exit the process if needed:
+    process.exit(1);
+});
+
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`app is listening on ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("database connection error : ", error)
+})
 
 
 
